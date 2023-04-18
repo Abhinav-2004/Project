9a0874a6b45f01b985eef842130b65a80c907585
Abhinav Singh <kittusinghranchi@gmail.com>
2023-04-19 02:26:50 +0530

    start from user object in index.js

--- api/index.js
+++ api/index.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const cors = require('cors');
+const {mongoose, models } = require('mongoose');
+
+
+require('dotenv').config();
 const app = express();
 
+
+
+app.use(express.json());
 app.use(cors({
     credentials:true,
     origin:'http://127.0.0.1:5173',
 }));
 
+mongoose.connect(process.env.MONGO_URL);
+
+
 app.get('/test',(req,res)=>{
     res.json("Test OK");
+});
+
+app.post('/register', (req,res)=>{
+    const {name,email,password} = req.body;
+
+    res.json({name,email,password});
 });
 
 app.listen(4000);
